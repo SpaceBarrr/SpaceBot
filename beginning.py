8ca074c16a1e70a4e64fd40f814e609f416ad08c
SpaceBarrr <SpaceBarrr@users.noreply.github.com>
2017-10-18 20:49:42 +0800

    learnt python
    lul

---
+++ beginning.py
@@ -0,0 +1,40 @@
+# print("hello world")
+
+# age = 20
+# name = 'Swaroop'
+# print('{0} was {1} years old when he wrote this book'.format(name, age))
+# print('Why is {0} playing with that python?'.format(name))
+
+# print('{0:.3f}'.format(1.0/3))
+# print('{0:_^11}'.format('hello'))
+# print('{name} wrote {book}'.format(name='Swaroop', book='A Byte of Python'))
+
+# i = 5
+# print(i)
+# i = i + 1
+# print(i)
+# s = '''This is a multi-line string.
+# This is the second line.'''
+# print(s)
+
+# number = 23
+# running = True
+# while running:
+# guess = int(input('Enter an integer : '))
+# if guess == number:
+# print('Congratulations, you guessed it.')
+# # this causes the while loop to stop
+# running = False
+# elif guess < number:
+# print('No, it is a little higher than that.')
+# else:
+# print('No, it is a little lower than that.')
+# else:
+# print('The while loop is over.')
+# # Do anything else you want to do here
+# print('Done')
+
+for i in range(1, 10):
+	print(i)
+else:
+	print('The number is 1')
