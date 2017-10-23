39490d9ae8cf18a5b1454197443efc1ce8134212
SpaceBarrr <SpaceBarrr@users.noreply.github.com>
2017-10-23 22:38:48 +0800

    started working on rng func

--- spacebot.py
+++ spacebot.py
@@ -1,9 +1,10 @@
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
+import random
 import logging
 #logging to console
-logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
 
 client = discord.Client()
 bot_prefix= "$" #prefix before bot commands
@@ -26,9 +27,24 @@ async def on_ready():
 async def ping(ctx):
 	await client.say("Pong!")
 
+#random numbers
+
+#this is just for debugging
+#print(x) 
+
 @client.command(pass_context=True)
-async def lock(ctx):
-	edit_channel(channel, **options)
-	
+async def ratewaifu(ctx):
+	if ctx.invoked_subcommand is None:
+		await client.say('Hmm, Id say {0.subcommand_passed} is a {number}'.format(ctx, number=random))
+
+@client.command(pass_context=True)
+async def cool(ctx):
+    if ctx.invoked_subcommand is None:
+        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
+
+#sean ford function
+@client.command(pass_context=True)
+async def seanford(ctx):
+	await client.say(":thinking: | Spacebar, I'd give sean ford a 1/10")
 
 client.run("MzcwMTM0Njc2MTg0MjM2MDM0.DMoiZw.ic9qPvPPBuspCynYaEkYyYf5xAk")
