b2967ae791be34f3f583675939bc54634ce8c3bf
SpaceBarrr <SpaceBarrr@users.noreply.github.com>
2017-10-24 20:42:52 +0800

    rng ratings

--- spacebot.py
+++ spacebot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import random
 import logging
 #logging to console
-#logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
 
 client = discord.Client()
 bot_prefix= "$" #prefix before bot commands
@@ -26,25 +26,23 @@ async def on_ready():
 @client.command(pass_context=True)
 async def ping(ctx):
 	await client.say("Pong!")
+	print("Ping function ran")
 
-#random numbers
-
-#this is just for debugging
-#print(x) 
-
+#takes the random number and "rates" someone out of 10
 @client.command(pass_context=True)
-async def ratewaifu(ctx):
-	if ctx.invoked_subcommand is None:
-		await client.say('Hmm, Id say {0.subcommand_passed} is a {number}'.format(ctx, number=random))
+async def ratewaifu(ctx, arg):
+	global x
+	x = random.randint(1, 10)
+	if 
 
-@client.command(pass_context=True)
-async def cool(ctx):
-    if ctx.invoked_subcommand is None:
-        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
+	print('Rate waifu args: {0}, Random number: {1}'.format(arg, x))
+	await client.say(':thinking: Hmm, Id say {0} is a {1}'.format(arg, x))
 
-#sean ford function
-@client.command(pass_context=True)
-async def seanford(ctx):
-	await client.say(":thinking: | Spacebar, I'd give sean ford a 1/10")
+
+	# if josephine in arg
+	# 	await client.say(':thinking: Hmm, Id say Josephine is a 10!')
+	# elif cian in arg
+	# 	await client.say(':thinking: Hmm, Id say Cian is a 10!')
+	# else
 
 client.run("MzcwMTM0Njc2MTg0MjM2MDM0.DMoiZw.ic9qPvPPBuspCynYaEkYyYf5xAk")
